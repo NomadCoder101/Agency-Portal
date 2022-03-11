8fc432da67c92cc4863a4a4808f72dfbaf38a7f5
cbinary07@gmail.com <cbinary07@gmail.com>
2022-03-11 03:09:22 -0500

    Some Changes

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+     path('testpage', TemplateView.as_view(template_name='pages/page.html')),
    # path('', views.index, name = 'index'),
      path('', views.index, {'pagename': ''}, name='home'),
      path('contact', views.contact, name='contact'),
