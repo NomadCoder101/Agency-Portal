0d5048c8cef7ba3bbfe9f74e0c86fd9c2b2d6904
cbinary07@gmail.com <cbinary07@gmail.com>
2022-03-11 23:47:24 -0500

    updates

--- agencyportal/urls.py
+++ agencyportal/urls.py
@@ -12,12 +12,20 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include, path
+from django.views.generic.base import TemplateView
+from quotes.views import Register
 
 urlpatterns = [
+    # path('testpage', TemplateView.as_view(template_name='pages/page.html')),
     path('admin/', admin.site.urls),
+    path('register/success/',TemplateView.as_view(template_name="registration/success.html"), name ='register-success'),
+    path('register/', Register.as_view(), name='register'),
     path('quote/', include('quotes.urls')),
-    path('',include('pages.urls')),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('pages.urls')),
+    
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,6 @@
-
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 from django.shortcuts import render
@@ -10,20 +12,33 @@ from pages.models import Page
 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class QuoteView(DetailView):
-        model = Quote
-        context_object_name = 'quote'
+
+
         
+
+class QuoteView(DetailView,LoginRequiredMixin):
+        login_url = reverse_lazy('login')
+        #model = Quote
+        context_object_name = 'quote'
+        def get_queryset(self):
+            return Quote.objects.filter(username=self.request.user)
+
         def get_context_data(self, **kwargs):
                 context = super(QuoteView, self).get_context_data(**kwargs)
                 context['page_list'] = Page.objects.all()
                 return context
 
 
-class QuoteList(ListView):
-    model = Quote
+class QuoteList(LoginRequiredMixin,ListView):
+    login_url = reverse_lazy('login')
+    #model = Quote
     context_object_name = 'all_quotes'
+
+    def get_queryset(self):
+        return Quote.objects.filter(username=self.request.user)
 
     def get_context_data(self, **kwargs):
         context = super(QuoteList, self).get_context_data(**kwargs)
@@ -31,11 +46,29 @@ class QuoteList(ListView):
         return context
 
 
+class Register(CreateView):
+    template_name = 'registration/register.html' 
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register-success')
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
+
+
+
+@login_required(login_url=reverse_lazy('login'))
 def quote_req(request):
+
     submitted = False
     if request.method == 'POST':
         form = QuoteForm(request.POST, request.FILES)
         if form.is_valid():
+            quote = form.save(commit=False)
+            try:
+                quote.username = request.user
+            except Exception:
+                pass
             form.save()
             return HttpResponseRedirect('/quote/?submitted=True')
     else:
@@ -43,3 +76,5 @@ def quote_req(request):
         if 'submitted' in request.GET:
             submitted = True
     return render(request, 'quotes/quote.html',{'form': form, 'page_list': Page.objects.all(), 'submitted': submitted})
+
+
